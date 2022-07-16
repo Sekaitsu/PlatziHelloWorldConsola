af5ab2e3b1f3b0f1cac590fb159e68692d37247c
SekaitsuSistecredito <lbernategordillo@gmail.com>
2022-07-16 14:09:30 -0500

    Platzi HelloWorld Consola

---
+++ helloWorldSiste/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace helloWorldSiste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD!");
+
+            string name = "Laura Valentina";
+            int age = 25;
+            int heigth = 163;
+            string information = "Hello user, the reguest info is: \nName: " + name +"\nAge: "+age+"\nHeigth: "+heigth;
+
+            Console.WriteLine(information); 
+        }
+    }
+}
